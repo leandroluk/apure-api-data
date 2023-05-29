8699ee06fa3ad34353c91ea47386de903a5ff596
Leandro Santiago Gomes <leandroluk@gmail.com>
2023-05-29 18:19:56 -0300

    test: finish integration tests

--- src/main/routes/api/workspace.routes.ts
+++ src/main/routes/api/workspace.routes.ts
@@ -4,18 +4,21 @@ import { Router } from "express";
 
 const workspaceRoutes = Router();
 
-workspaceRoutes.delete("/:_id", async (req, res) => {
-  const data = await validators.disableWorkspaceValidator(req);
-  await factories.disableWorkspaceFactory().disable(data);
-  res.sendStatus(204);
-});
-
+// editWorkspace
 workspaceRoutes.put("/:_id", async (req, res) => {
   const data = await validators.editWorkspaceValidator(req);
   const result = await factories.editWorkspaceFactory().edit(data);
   res.json(result);
 });
 
+// disableWorkspace
+workspaceRoutes.delete("/:_id", async (req, res) => {
+  const data = await validators.disableWorkspaceValidator(req);
+  await factories.disableWorkspaceFactory().disable(data);
+  res.sendStatus(204);
+});
+
+// addWorkspace
 workspaceRoutes.post("/", async (req, res) => {
   const data = await validators.addWorkspaceValidator(req);
   const result = await factories.addWorkspaceFactory().add(data);
